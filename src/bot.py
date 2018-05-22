7af535360312a8c663947a9761c76717fa65e360
Elliott Pardee <me@vypr.xyz>
2018-05-22 04:59:11 -0400

    Fix shard outputs.

--- src/bot.py
+++ src/bot.py
@@ -65,8 +65,6 @@ class BibleBot(discord.AutoShardedClient):
 
     async def run_timed_votds(self):
         await self.wait_until_ready()
-        activity = discord.Game(central.version + " | Shard: " + str(self.shard_id) + " / " + str(self.shard_count))
-        await self.change_presence(status=discord.Status.online, activity=activity)
 
         results = [x for x in central.guildDB.all() if "channel" in x and "time" in x]
 
