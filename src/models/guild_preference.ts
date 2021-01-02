2d73b2ba103e646dc37f0c56f1599cb786bb4abf
Seraphim R.P <me@srp.life>
2021-01-01 23:17:03 -0500

    Remove extraneous data in GuildPreference.

--- src/models/guild_preference.ts
+++ src/models/guild_preference.ts
@@ -2,11 +2,10 @@ import * as mongoose from 'mongoose';
 
 const GuildPreferenceSchema: mongoose.Schema = new mongoose.Schema({
     guild: { type: String, required: true },
-    input: { type: String, required: true },
+    prefix: { type: String, required: true },
+    ignoringBrackets: { type: String, required: true },
     language: { type: String, required: true },
-    version: { type: String, required: true },
-    headings: { type: Boolean, required: true },
-    verseNumbers: { type: Boolean, required: true }
+    version: { type: String, required: true }
 });
 
 export default mongoose.model('GuildPreference', GuildPreferenceSchema);
