29980b08e15d0ca29109fc263455b6d0f3b99ece
Seraphim R.P <me@srp.life>
2021-01-17 06:48:13 -0500

    Fix formula for B to MB?

--- src/tasks/memory_monitor.ts
+++ src/tasks/memory_monitor.ts
@@ -4,6 +4,6 @@ import { log } from '../helpers/logger';
 
 export const startMemoryMonitor = (bot: Client): void => {
     cron.schedule('45 * * * * *', () => {
-        log('info', bot.shard.ids[0], `memory usage - ${Math.round((process.memoryUsage().rss / 1024) / 102400) / 100} MB`);        
+        log('info', bot.shard.ids[0], `memory usage - ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100} MB`);        
     }).start();
 };
