2a6f405eb84042018649b5e7520d43be1d5120ee
Seraphim R.P <srp@kerygma.digital>
2021-05-16 06:15:22 -0400

    Add code coverage etc.

---
+++ test/BibleBot.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BibleBot.Backend.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
