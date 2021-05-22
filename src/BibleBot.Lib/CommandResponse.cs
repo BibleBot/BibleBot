fd5954a60591e3b60594e61a4c59e1a10dd83bb9
Seraphim R.P <srp@kerygma.digital>
2021-05-22 19:45:45 -0400

    It'd probably be useful if use the response interface.

--- src/BibleBot.Lib/CommandResponse.cs
+++ src/BibleBot.Lib/CommandResponse.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace BibleBot.Lib
 {
-    public class CommandResponse
+    public class CommandResponse: IResponse
     {
         public bool OK { get; set; }
         public List<InternalEmbed> Pages { get; set; }
--- src/BibleBot.Lib/VerseResponse.cs
+++ src/BibleBot.Lib/VerseResponse.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace BibleBot.Lib
 {
-    public class VerseResponse
+    public class VerseResponse: IResponse
     {
         public bool OK { get; set; }
         public List<Verse> Verses { get; set; }
