49586d74b99ec484e4d899a4e2010d148fac1265
Seraphim R.P <srp@kerygma.digital>
2021-05-23 17:44:14 -0400

    Implement embedify function on frontend.

--- src/BibleBot.Frontend/Utils.cs
+++ src/BibleBot.Frontend/Utils.cs
@@ -5,11 +5,15 @@ namespace BibleBot.Frontend
 {
     public class Utils
     {
+        public enum Colours
+        {
+            NORMAL_COLOR = 6709986,
+            ERROR_COLOR = 16723502
+        }
+
         public DiscordEmbed Embed2Embed(InternalEmbed embed)
         {
             var builder = new DiscordEmbedBuilder();
-
-            var footerText = 
 
             builder.WithTitle(embed.Title);
             builder.WithDescription(embed.Description);
@@ -19,6 +23,32 @@ namespace BibleBot.Frontend
             if (embed.Author != null)
             {
                 builder.WithAuthor(embed.Author.Name, null, null);
+            }
+
+            return builder.Build();
+        }
+
+
+        public DiscordEmbed Embedify(string title, string description, bool isError)
+        {
+            return Embedify(null, title, description, isError, null);
+        }
+
+        public DiscordEmbed Embedify(string author, string title, string description, bool isError, string copyright)
+        {
+            // TODO: Do not use hard-coded version tags.
+            string footerText = "BibleBot v9.1-beta by Kerygma Digital";
+
+            var builder = new DiscordEmbedBuilder();
+            builder.WithTitle(title);
+            builder.WithDescription(description);
+            builder.WithColor(isError ? (int) Colours.ERROR_COLOR : (int) Colours.NORMAL_COLOR);
+
+            builder.WithFooter(footerText, "https://i.imgur.com/hr4RXpy.png");
+
+            if (author != null)
+            {
+                builder.WithAuthor(author, null, null);
             }
 
             return builder.Build();
