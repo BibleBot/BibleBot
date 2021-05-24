a2f4c431d2fb6e6f974d6b4ff7cd7b10c003fd64
Seraphim R.P <srp@kerygma.digital>
2021-05-24 15:00:56 -0400

    Utilize a background task for message events to prevent deadlock.

--- src/BibleBot.Frontend/Program.cs
+++ src/BibleBot.Frontend/Program.cs
@@ -30,55 +30,60 @@ namespace BibleBot.Frontend
             var cli = new RestClient("http://localhost:5000");
             var acceptablePrefixes = new List<string>{ "+", "-", "!", "=", "$", "%", "^", "*", ".", ",", "?", "~", "|" };
             
-            bot.MessageCreated += async (s, e) =>
+            bot.MessageCreated += (s, e) =>
             {
-                if (e.Author.Id.ToString() != "186046294286925824" || e.Author == bot.CurrentUser)
+                _ = Task.Run(async () =>
                 {
-                    return;
-                }
-
-                var authorAsMember = await e.Guild.GetMemberAsync(e.Author.Id);
-                var requestObj = new BibleBot.Lib.Request
-                {
-                        UserId = e.Author.Id.ToString(),
-                        UserPermissions = (long) authorAsMember.PermissionsIn(e.Channel),
-                        GuildId = e.Guild.Id.ToString(),
-                        IsDM = e.Channel.IsPrivate,
-                        Body = e.Message.Content,
-                        Token = Environment.GetEnvironmentVariable("ENDPOINT_TOKEN")
-                };
-                IResponse response;
-
-                if (acceptablePrefixes.Contains(e.Message.Content.ElementAtOrDefault(0).ToString()))
-                {
-
-                    var request = new RestRequest("api/commands/process");
-                    request.AddJsonBody(requestObj);
-
-                    response = await cli.PostAsync<CommandResponse>(request);
-                }
-                else
-                {
-                    var request = new RestRequest("api/verses/process");
-                    request.AddJsonBody(requestObj);
-
-                    response = await cli.PostAsync<VerseResponse>(request);
-                }
-
-                if (response.GetType().Equals(typeof(CommandResponse)))
-                {
-                    await e.Message.RespondAsync(utils.Embed2Embed((response as CommandResponse).Pages[0]));
-                }
-                else if (response.GetType().Equals(typeof(VerseResponse)))
-                {
-                    var verseResp = response as VerseResponse;
-
-                    foreach (Verse verse in verseResp.Verses)
+                    if (e.Author.Id.ToString() != "186046294286925824" || e.Author == bot.CurrentUser)
                     {
-                        var embed = utils.Embedify($"{verse.Reference.ToString()} - {verse.Reference.Version.Name}", verse.Title, verse.Text, false, null);
-                        await e.Message.RespondAsync(embed);
+                        return;
                     }
-                }
+
+                    var authorAsMember = await e.Guild.GetMemberAsync(e.Author.Id);
+                    var requestObj = new BibleBot.Lib.Request
+                    {
+                            UserId = e.Author.Id.ToString(),
+                            UserPermissions = (long) authorAsMember.PermissionsIn(e.Channel),
+                            GuildId = e.Guild.Id.ToString(),
+                            IsDM = e.Channel.IsPrivate,
+                            Body = e.Message.Content,
+                            Token = Environment.GetEnvironmentVariable("ENDPOINT_TOKEN")
+                    };
+                    IResponse response;
+
+                    if (acceptablePrefixes.Contains(e.Message.Content.ElementAtOrDefault(0).ToString()))
+                    {
+
+                        var request = new RestRequest("api/commands/process");
+                        request.AddJsonBody(requestObj);
+
+                        response = await cli.PostAsync<CommandResponse>(request);
+                    }
+                    else
+                    {
+                        var request = new RestRequest("api/verses/process");
+                        request.AddJsonBody(requestObj);
+
+                        response = await cli.PostAsync<VerseResponse>(request);
+                    }
+
+                    if (response.GetType().Equals(typeof(CommandResponse)))
+                    {
+                        await e.Message.RespondAsync(utils.Embed2Embed((response as CommandResponse).Pages[0]));
+                    }
+                    else if (response.GetType().Equals(typeof(VerseResponse)))
+                    {
+                        var verseResp = response as VerseResponse;
+
+                        foreach (Verse verse in verseResp.Verses)
+                        {
+                            var embed = utils.Embedify($"{verse.Reference.ToString()} - {verse.Reference.Version.Name}", verse.Title, verse.Text, false, null);
+                            await e.Message.RespondAsync(embed);
+                        }
+                    }
+                });
+
+                return Task.CompletedTask;
             };
 
             await bot.ConnectAsync();
