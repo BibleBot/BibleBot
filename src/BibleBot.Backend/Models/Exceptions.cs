af7366359adbad9e99c2002ee58ed1cbf6f71794
Seraphim R.P <srp@kerygma.digital>
2021-06-17 14:16:17 -0400

    Implement a language service.

--- src/BibleBot.Backend/Models/Exceptions.cs
+++ src/BibleBot.Backend/Models/Exceptions.cs
@@ -10,4 +10,15 @@ namespace BibleBot.Backend.Models
             System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
+
+    [System.Serializable]
+    public class StringNotFoundException : System.Exception
+    {
+        public StringNotFoundException() { }
+        public StringNotFoundException(string message) : base(message) { }
+        public StringNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+        protected StringNotFoundException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
 }
