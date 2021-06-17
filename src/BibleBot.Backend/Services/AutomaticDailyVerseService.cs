6259ac7c1fb225878f2500ffc630e4634640bd32
Seraphim R.P <srp@kerygma.digital>
2021-06-16 23:40:37 -0400

    Fix potential duplication bug with automatic daily verse.

--- src/BibleBot.Backend/Services/AutomaticDailyVerseService.cs
+++ src/BibleBot.Backend/Services/AutomaticDailyVerseService.cs
@@ -51,6 +51,7 @@ namespace BibleBot.Backend.Services
         {
             var count = 0;
             var idealCount = 0;
+            var guildsCleared = new List<string>();
 
             Instant currentInstant = SystemClock.Instance.GetCurrentInstant();
             ZonedDateTime dateTimeInStandardTz = currentInstant.InZone(DateTimeZoneProviders.Tzdb["America/Detroit"]);
@@ -80,33 +81,44 @@ namespace BibleBot.Backend.Services
 
             foreach (Guild guild in matches)
             {
-                var version = guild.Version != null ? guild.Version : "RSV";
-                var idealVersion = _versionService.Get(version);
-
-                if (idealVersion == null)
+                if (!guildsCleared.Contains(guild.GuildId))
                 {
-                    idealVersion = _versionService.Get("RSV");
-                }
+                    var version = guild.Version != null ? guild.Version : "RSV";
+                    var idealVersion = _versionService.Get(version);
 
-                string votdRef = _bgProvider.GetDailyVerse().GetAwaiter().GetResult();
-                Verse verse = _bgProvider.GetVerse(votdRef, true, true, idealVersion).GetAwaiter().GetResult();
+                    if (idealVersion == null)
+                    {
+                        idealVersion = _versionService.Get("RSV");
+                    }
 
-                var embed = new Utils().Embedify($"{verse.Reference.AsString} - {verse.Reference.Version.Name}", verse.Title, verse.Text, false, null);
-                var webhookRequestBody = new WebhookRequestBody
-                {
-                    Content = "Here is the daily verse:",
-                    Username = "BibleBot Automatic Daily Verses",
-                    AvatarURL = embed.Footer.IconURL,
-                    Embeds = new List<InternalEmbed> { embed }
-                };
+                    string votdRef = _bgProvider.GetDailyVerse().GetAwaiter().GetResult();
 
-                var request = new RestRequest(guild.DailyVerseWebhook);
-                request.AddJsonBody(webhookRequestBody);
+                    IBibleProvider provider = _bgProvider;
 
-                var resp = await _restClient.ExecuteAsync(request, Method.POST);
-                if (resp.StatusCode == System.Net.HttpStatusCode.NoContent)
-                {
-                    count += 1;
+                    if (provider != null)
+                    {
+                        Verse verse = provider.GetVerse(votdRef, true, true, idealVersion).GetAwaiter().GetResult();
+
+                        var embed = new Utils().Embedify($"{verse.Reference.AsString} - {verse.Reference.Version.Name}", verse.Title, verse.Text, false, null);
+                        var webhookRequestBody = new WebhookRequestBody
+                        {
+                            Content = "Here is the daily verse:",
+                            Username = "BibleBot Automatic Daily Verses",
+                            AvatarURL = embed.Footer.IconURL,
+                            Embeds = new List<InternalEmbed> { embed }
+                        };
+
+                        var request = new RestRequest(guild.DailyVerseWebhook);
+                        request.AddJsonBody(webhookRequestBody);
+
+                        var resp = await _restClient.ExecuteAsync(request, Method.POST);
+                        if (resp.StatusCode == System.Net.HttpStatusCode.NoContent)
+                        {
+                            count += 1;
+                        }
+                    }
+
+                    guildsCleared.Add(guild.Id);
                 }
             }
 
