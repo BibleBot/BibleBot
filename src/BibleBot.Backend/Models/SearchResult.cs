83b4d786fc72c10a2e7921dc2371a22bf73ebac6
Seraphim R.P <srp@kerygma.digital>
2021-06-22 19:55:23 -0400

    Add license notices.

--- src/BibleBot.Backend/Models/SearchResult.cs
+++ src/BibleBot.Backend/Models/SearchResult.cs
@@ -1,3 +1,11 @@
+/*
+* Copyright (C) 2016-2021 Kerygma Digital Co.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
 namespace BibleBot.Backend.Models
 {
     public class SearchResult
