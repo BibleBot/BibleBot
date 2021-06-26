45e192c766ecf79e84f336910431da8ecb643a79
Seraphim R.P <me@srp.life>
2021-06-25 23:13:12 -0400

    Begin implementing unit tests.

---
+++ test/BibleBot.Backend.Tests/VersesControllerTest.cs
@@ -0,0 +1,113 @@
+/*
+* Copyright (C) 2016-2021 Kerygma Digital Co.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+using System;
+using System.Collections.Generic;
+using BibleBot.Backend.Controllers;
+using BibleBot.Backend.Models;
+using BibleBot.Backend.Services;
+using BibleBot.Backend.Services.Providers;
+using BibleBot.Backend.Tests.Mocks;
+using BibleBot.Lib;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace BibleBot.Backend.Tests
+{
+    [TestFixture, Category("VersesController")]
+    public class VersesControllerTest
+    {
+        private VersesController versesController;
+
+        private VersionService versionService;
+
+        private Mock<UserService> userServiceMock;
+        private Mock<GuildService> guildServiceMock;
+        private Mock<ParsingService> parsingServiceMock;
+        private Mock<NameFetchingService> nameFetchingServiceMock;
+
+        private Mock<BibleGatewayProvider> bgProviderMock;
+        private Mock<APIBibleProvider> abProviderMock;
+
+        private IDatabaseSettings databaseSettings;
+        private Lib.Version testVersion;
+
+        [SetUp]
+        public void Setup()
+        {
+            databaseSettings = new DatabaseSettings
+            {
+                UserCollectionName = "Users",
+                GuildCollectionName = "Guilds",
+                VersionCollectionName = "Versions",
+                LanguageCollectionName = "Languages",
+                FrontendStatsCollectionName = "FrontendStats",
+                DatabaseName = "BibleBotBackend"
+            };
+
+            userServiceMock = new Mock<UserService>(databaseSettings);
+            guildServiceMock = new Mock<GuildService>(databaseSettings);
+            versionService = new VersionService(databaseSettings);
+            parsingServiceMock = new Mock<ParsingService>(versionService);
+            nameFetchingServiceMock = new Mock<NameFetchingService>();
+
+            bgProviderMock = new Mock<BibleGatewayProvider>();
+            abProviderMock = new Mock<APIBibleProvider>();
+
+            testVersion = versionService.Get("RSV");
+
+            if (testVersion == null)
+            {
+                testVersion = versionService.Create(new MockVersion());
+            }
+
+            versesController = new VersesController(userServiceMock.Object, guildServiceMock.Object,
+                                                    parsingServiceMock.Object, versionService,
+                                                    nameFetchingServiceMock.Object, bgProviderMock.Object,
+                                                    abProviderMock.Object);
+        }
+
+        [Test]
+        public void BibleGatewayVerses()
+        {
+            var resp = versesController.ProcessMessage(new MockRequest("Genesis 1:1 RSV")).GetAwaiter().GetResult();
+
+            var expected = new VerseResponse
+            {
+                OK = true,
+                LogStatement = "Genesis 1:1",
+                DisplayStyle = "embed",
+                Verses = new List<Verse>
+                {
+                    new Verse
+                    {
+                        Title = "Six Days of Creation and the Sabbath",
+                        PsalmTitle = "",
+                        Text = "<**1**> In the beginning God created the heavens and the earth.",
+                        Reference = new Reference
+                        {
+                            Book = "Genesis",
+                            StartingChapter = 1,
+                            StartingVerse = 1,
+                            EndingChapter = 1,
+                            EndingVerse = 1,
+                            Version = testVersion,
+                            IsOT = true,
+                            IsNT = false,
+                            IsDEU = false,
+                            AsString = "Genesis 1:1"
+                        }
+                    }
+                }
+            };
+
+            resp.Should().BeEquivalentTo(expected);
+        }
+    }
+}
