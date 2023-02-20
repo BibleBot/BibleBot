1e9c3061a172beba52674da66a885ac0c24e1cae
Seraphim R.P <me@srp.life>
2023-02-20 16:47:52 -0500

    Renew copyrights.

--- src/BibleBot.Backend/Models/Request.cs
+++ src/BibleBot.Backend/Models/Request.cs
@@ -1,5 +1,5 @@
 ﻿/*
-* Copyright (C) 2016-2022 Kerygma Digital Co.
+* Copyright (C) 2016-2023 Kerygma Digital Co.
 *
 * This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this file,
