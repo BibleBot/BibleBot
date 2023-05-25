18a8f54193f3934d67f947c4afcb673eaeaa38b6
Seraphim R.P <me@srp.life>
2023-05-24 22:59:20 -0400

    Forgot to leave another character in the short hash.

--- src/BibleBot.Backend/Controllers/CommandGroups/Information/InformationCommandGroup.cs
+++ src/BibleBot.Backend/Controllers/CommandGroups/Information/InformationCommandGroup.cs
@@ -90,7 +90,7 @@ namespace BibleBot.Backend.Controllers.CommandGroups.Information
                 $"**Guild Preference Count**: {guildPrefs}\n" +
                 $"**Version Count**: {versions}\n\n" +
                 $"**__Metadata__**\n" +
-                $"**Frontend**: v{Utils.Version} ([{frontendStats.FrontendRepoCommitHash.Substring(0, 7)}](https://gitlab.com/kerygmadigital/biblebot/BibleBot/-/commit/{frontendStats.FrontendRepoCommitHash}))\n" +
+                $"**Frontend**: v{Utils.Version} ([{frontendStats.FrontendRepoCommitHash.Substring(0, 8)}](https://gitlab.com/kerygmadigital/biblebot/BibleBot/-/commit/{frontendStats.FrontendRepoCommitHash}))\n" +
                 $"**Backend**: v{Utils.Version} ([{ThisAssembly.Git.Commit}](https://gitlab.com/kerygmadigital/biblebot/BibleBot/-/commit/{ThisAssembly.Git.Sha}))\n";
 
 
