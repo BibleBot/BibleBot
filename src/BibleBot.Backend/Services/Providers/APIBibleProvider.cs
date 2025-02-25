75bd00aeec944238046da42c7705faa9af2cca3f
Seraphim Pardee <me@srp.life>
2025-02-25 16:18:37 -0500

    fix: patch content error in Galatians 3:24 NLD1939

--- src/BibleBot.Backend/Services/Providers/APIBibleProvider.cs
+++ src/BibleBot.Backend/Services/Providers/APIBibleProvider.cs
@@ -195,6 +195,11 @@ namespace BibleBot.Backend.Services.Providers
 
             string text = string.Join("\n", texts);
 
+            if (reference.Version.Abbreviation == "NLD1939" && text.Contains("tuchtmeester geweest tot Christus' 3:opdat we"))
+            {
+                text = text.Replace("tuchtmeester geweest tot Christus' 3:opdat we", "tuchtmeester geweest tot Christus' komst, opdat we");
+            }
+
             // As the verse reference could have a non-English name...
             string bookUrl = string.Format(_getBookURI, reference.Version.ApiBibleId, resp.Passages[0].BookId);
             ABBookData bookResp = await _cachingHttpClient.GetJsonContentAs<ABBookData>(bookUrl, _jsonOptions);
