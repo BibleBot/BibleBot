5cdaf8cad6227366c608c91bd4d5aa00877d8ecc
Seraphim R. Pardee <me@srp.life>
2025-09-30 00:59:55 -0400

    test: make sure tests work with metrics

--- test/BibleBot.Tests.Backend/TestBaseClass.cs
+++ test/BibleBot.Tests.Backend/TestBaseClass.cs
@@ -15,8 +15,10 @@ using BibleBot.Backend.Services.Providers;
 using BibleBot.Backend.Services.Providers.Content;
 using BibleBot.Models;
 using BibleBot.Tests.Backend.Mocks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -36,6 +38,7 @@ namespace BibleBot.Tests.Backend
         private PreferenceService _preferenceService;
         protected VersionService _versionService;
         private LanguageService _languageService;
+        private VerseMetricsService _verseMetricsService;
 
         private Mock<UserService> _userServiceMock;
         private Mock<GuildService> _guildServiceMock;
@@ -75,12 +78,22 @@ namespace BibleBot.Tests.Backend
                 Configuration = "127.0.0.1:6379"
             }));
 
+            ServiceCollection serviceCollection = new();
+            serviceCollection.AddDbContextPool<MetricsContext>(options =>
+                options.UseNpgsql(
+                    System.Environment.GetEnvironmentVariable("POSTGRES_CONN"),
+                    o => o.SetPostgresVersion(18, 0)
+                          .UseNodaTime()
+                )
+            );
+
             _mongoService = new MongoService(_databaseSettings);
             _preferenceService = new PreferenceService(_cache, _mongoService);
             _userServiceMock = new Mock<UserService>(_preferenceService);
             _guildServiceMock = new Mock<GuildService>(_preferenceService);
             _versionService = new VersionService(_mongoService);
             _languageService = new LanguageService(_mongoService);
+            _verseMetricsService = new VerseMetricsService(serviceCollection.BuildServiceProvider());
             _resourceServiceMock = new Mock<ResourceService>();
             _parsingServiceMock = new Mock<ParsingService>(false);
             _frontendStatsServiceMock = new Mock<FrontendStatsService>(_mongoService);
@@ -103,7 +116,7 @@ namespace BibleBot.Tests.Backend
                                                     specialVerseProcessingService, bibleProviders, _localizerFactory);
 
             _versesController = new VersesController(_userServiceMock.Object, _guildServiceMock.Object,
-                                                    _parsingServiceMock.Object, _versionService, _languageService,
+                                                    _parsingServiceMock.Object, _verseMetricsService, _versionService, _languageService,
                                                     _metadataFetchingServiceMock.Object, bibleProviders, new StringLocalizer<VersesController>(_localizerFactory), new StringLocalizer<SharedResource>(_localizerFactory));
         }
     }
