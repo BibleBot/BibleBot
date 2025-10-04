7e04518036d465311fee8456790e75c4d512b88e
Seraphim R. Pardee <me@srp.life>
2025-10-04 01:30:35 -0400

    chore: add publisher column to most popular versions query

--- sql/most_pop_versions.sql
+++ sql/most_pop_versions.sql
@@ -1,10 +1,12 @@
 SELECT version,
+       publisher,
        COUNT(*) AS reference_count
 FROM
     (SELECT version
+            publisher,
      FROM verse_metrics
-     UNION ALL SELECT m.version
+     UNION ALL SELECT m.version, m.publisher
      FROM appended_verses a
      JOIN verse_metrics m ON a.verse_metric_id = m.id) AS all_verses
-GROUP BY version
+GROUP BY version, publisher
 ORDER BY reference_count DESC
