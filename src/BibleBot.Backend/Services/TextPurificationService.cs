f7cc4ee9a89532a56ba4bd1866b820cb5e24af7d
Seraphim Pardee <me@srp.life>
2026-06-05 20:44:14 -0400

    fix(backend): handle another odd text quirk of the CSB

--- src/BibleBot.Backend/Services/TextPurificationService.cs
+++ src/BibleBot.Backend/Services/TextPurificationService.cs
@@ -31,6 +31,7 @@ namespace BibleBot.Backend.Services
             { "||" , "" },
             { "`", "'" },
             { "\\*(Selah)\\*", "*(Selah)*"},
+            { "#— #", "— "},
             { " ", " " }
         };
 
